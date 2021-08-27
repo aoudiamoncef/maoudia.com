8a313e5d789fc263c4b5a1c2d284905508345c31
Moncef AOUDIA <mf.aoudia@gmail.com>
2021-08-27 23:36:04 +0200

    fix: wrong baseUrl

--- cloudflare-pages.sh
+++ cloudflare-pages.sh
@@ -4,9 +4,9 @@ DEFAULT_URL="/"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ "$BRANCH" == "main" ]]; then
-  SITE_URL="www.maoudia.com"
+  SITE_URL=" https://www.maoudia.com"
 fi
 
 echo "Current baseUrl: ${SITE_URL:-$DEFAULT_URL}"
 
-gem install asciidoctor asciidoctor-html5s && hugo -b https://${SITE_URL:-$DEFAULT_URL} --minify
+gem install asciidoctor asciidoctor-html5s && hugo -b ${SITE_URL:-$DEFAULT_URL} --minify
