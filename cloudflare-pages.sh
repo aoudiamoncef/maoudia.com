493a1a1a701354c938fe3455f7ef779169487242
Moncef AOUDIA <mf.aoudia@gmail.com>
2021-08-27 22:40:20 +0200

    refactor: simplify CloudFlare Pages script

--- cloudflare-pages.sh
+++ cloudflare-pages.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+DEFAULT_URL="/"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ "$BRANCH" == "main" ]]; then
   echo "Build && deploy main branch"
   SITE_URL="www.maoudia.com"
-  echo "Production baseUrl: ${SITE_URL}"
-  gem install asciidoctor asciidoctor-html5s && hugo -b https://${SITE_URL} --minify
 else
   echo "Build && deploy $BRANCH"
   DOMAIN="maoudia.pages.dev"
   COMMIT_SHA=$(git rev-parse HEAD)
   SUBDOMAIN=${COMMIT_SHA:0:7}
   SITE_URL="${SUBDOMAIN}.${DOMAIN}"
-  echo "CloudFlare Pages developement baseUrl: ${SITE_URL}"
-  gem install asciidoctor asciidoctor-html5s && hugo -b https://${SITE_URL} --minify
 fi
+
+echo "Current baseUrl: ${SITE_URL:-$DEFAULT_URL}"
+
+gem install asciidoctor asciidoctor-html5s && hugo -b https://${SITE_URL:-$DEFAULT_URL} --minify
