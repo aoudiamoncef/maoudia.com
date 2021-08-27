2cd1ce1231172b752d1162901f32cd4e81e4eb00
Moncef AOUDIA <mf.aoudia@gmail.com>
2021-08-27 23:28:11 +0200

    fix: remove DEV url detection

--- cloudflare-pages.sh
+++ cloudflare-pages.sh
@@ -4,13 +4,7 @@ DEFAULT_URL="/"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ "$BRANCH" == "main" ]]; then
-  echo "Build && deploy main branch"
   SITE_URL="www.maoudia.com"
-else
-  echo "Build && deploy $BRANCH"
-  DOMAIN="maoudia.pages.dev"
-  SUBDOMAIN=${CF_PAGES_COMMIT_SHA:0:8}
-  SITE_URL="${SUBDOMAIN}.${DOMAIN}"
 fi
 
 echo "Current baseUrl: ${SITE_URL:-$DEFAULT_URL}"
