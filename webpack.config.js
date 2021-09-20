5cdeba4334fee94652ce35a6a91e7a178c1660b6
Moncef AOUDIA <mf.aoudia@gmail.com>
2021-09-21 00:30:23 +0200

    refactor: global enhancements

--- webpack.config.js
+++ webpack.config.js
@@ -16,42 +16,17 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: "**/about/**/*.html",
-          to: path.resolve(__dirname, "dist"),
-          context: "public/",
-        },
-        {
           from: "**/offline/**/*.html",
           to: path.resolve(__dirname, "dist"),
           context: "public/",
         },
         {
-          from: "**/uses/**/*.{html,ico,svg,png}",
+          from: "*.{html,ico}",
           to: path.resolve(__dirname, "dist"),
           context: "public/",
-        },
-        {
-          from: "*.{html,json,ico,svg,png,js,css,woff2}",
-          to: path.resolve(__dirname, "dist"),
-          context: "public/",
-        },
-        {
-          from: "fr/*.{html,ico,svg,png}",
-          to: path.resolve(__dirname, "dist"),
-          context: "public/",
-        },
-        {
-          from: "images/badges/*-large*.{ico,svg,png}",
-          to: path.resolve(__dirname, "dist"),
-          context: "public/",
-        },  
+        }, 
         {
           from: "images/offline/*.{ico,svg,png}",
-          to: path.resolve(__dirname, "dist"),
-          context: "public/",
-        },
-        {
-          from: "images/favicons/*.{ico,svg,png,xml}",
           to: path.resolve(__dirname, "dist"),
           context: "public/",
         },
@@ -62,7 +37,7 @@ module.exports = {
       mode: 'development',
       swSrc: "./pwa/service-worker.js",
       swDest: "./service-worker.js",
-      exclude: ["service-worker.bundle.js", "sw.js", "amp/**"],
+      exclude: ["service-worker.bundle.js", "sw.js"],
       maximumFileSizeToCacheInBytes: 6710886,
     }),
     new webpack.ProgressPlugin(),
